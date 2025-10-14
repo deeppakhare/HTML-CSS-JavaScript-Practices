9eb21f089b124cf30549ba904ac15c190a90ac6a
justforit4102 <deeppakhare19@gmail.com>
2025-10-14 22:05:18 +0530

    adding CSS Folder

---
+++ CSS3/Box Model/TransformationExercise.js
@@ -0,0 +1,7 @@
+var my_canvas = document.getElementById("mycanvas");
+var my_context = my_canvas.getContext("2d");
+my_context.fillStyle="black";
+my_context.fillRect(10, 10, 1290, 50);
+my_context.fillStyle = "blue";
+my_context.font = "bold 46px chiller";
+my_context.fillText("The Mega Store...", 500, 50);
