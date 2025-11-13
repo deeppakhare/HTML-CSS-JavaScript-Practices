7d70250fadf46bfdc4c7fc378c41407e8092fe6c
justforit4102 <deeppakhare19@gmail.com>
2025-11-13 12:17:41 +0530

    DOM second Project

--- JAVASCRIPT/DOM/PROJECTS/Project-02/02.js
+++ JAVASCRIPT/DOM/PROJECTS/Project-02/02.js
@@ -13,6 +13,16 @@ form.addEventListener("submit", function(event) {
         results.innerHTML = 'Please give a valid weight';
     } else {
         const BMI = (weight / ((height * height) / 10000)).toFixed(2);
-        results.innerHTML = `<span>${BMI}</span>`
+        let status = ''
+        if (BMI <= 18.6) {
+            status = 'you are under weight'
+        } else if (BMI >= 18.6 && BMI <= 24.9) {
+            status = `you are in normal range`
+        } else {
+            status = `you are overweight`
+        }
+        results.innerHTML = `<h3>${BMI}</h3><p>${status}</p>`
     }
+    
 })
+
