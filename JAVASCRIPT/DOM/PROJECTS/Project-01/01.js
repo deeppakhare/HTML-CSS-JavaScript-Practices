40a1368b04dc7332b4cff7eeb8a7e595301bce32
justforit4102 <deeppakhare19@gmail.com>
2025-11-13 11:27:41 +0530

    DOM second Project

--- JAVASCRIPT/DOM/PROJECTS/Project-01/01.js
+++ JAVASCRIPT/DOM/PROJECTS/Project-01/01.js
@@ -1,2 +1,42 @@
-const buttons = document.querySelectorAll('.button')
-console.log(buttons)
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+
+buttons.forEach(function(button){
+    console.log(button)
+    button.addEventListener("click", function (event) {
+        console.log(event)
+        console.log(event.target)
+        if (event.target.id === 'grey') {
+            body.style.backgroundColor = event.target.id;
+        }  
+        if (event.target.id === 'white'){
+            body.style.backgroundColor = event.target.id;
+        }  
+        if (event.target.id === 'blue'){
+            body.style.backgroundColor = event.target.id;
+        }  
+        if (event.target.id === 'yellow'){
+            body.style.backgroundColor = event.target.id;
+        }
+        
+        // switch (event.target.id ) {
+        //     case 'grey':
+        //         body.style.backgroundColor = event.target.id;
+        //         break;
+        //     case 'white':
+        //         body.style.backgroundColor = event.target.id;
+        //         break;
+        //     case 'blue':
+        //         body.style.backgroundColor = event.target.id;
+        //         break;
+        //     case 'yellow':
+        //         body.style.backgroundColor = event.target.id;
+        //         break;
+        
+        //     default:
+        //         console.log(`unknown error`, event.target.id)
+                
+        // }
+    });
+});
