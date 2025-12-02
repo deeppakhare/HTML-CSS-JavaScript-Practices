e3dd95ba8d7933b82baac187f4f114613620ba2f
justforit4102 <deeppakhare19@gmail.com>
2025-12-02 13:20:33 +0530

    learning Object oriented programming

---
+++ JAVASCRIPT/OOP/oop.js
@@ -0,0 +1,17 @@
+const user = {
+    username: "deep",
+    loginCount: 8,
+    signedIn: true,
+
+
+    getUserDetails: function(){
+        // console.log("Got user details from database");
+        console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+}
+
+console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
