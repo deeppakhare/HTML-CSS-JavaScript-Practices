aae3e21c84e61d04780ccaaf73554c52a64c7d7f
justforit4102 <deeppakhare19@gmail.com>
2025-12-03 18:23:58 +0530

    learning oop

--- JAVASCRIPT/OOP/oop.js
+++ JAVASCRIPT/OOP/oop.js
@@ -11,7 +11,25 @@ const user = {
     }
 }
 
-console.log(user.username);
+// console.log(user.username);
 // console.log(user.getUserDetails());
 // console.log(this);
 
+
+
+function User(username, logiCount, isloggedIn) {
+    this.username = username;
+    this.logiCount = logiCount;
+    this.isloggedIn = isloggedIn;
+    
+    this.greeting = function (){
+        console.log(`Welcome ${username}`);
+    }
+
+    //return this
+}
+
+const userOne = new User("deep", 12, true)
+const userTwo = new User("Deepak", 32, false)
+console.log(userOne.constructor)
+// console.log(userTwo)
