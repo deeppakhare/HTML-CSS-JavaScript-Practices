3cabce3bae3935f8405da91d80f10133503b7d73
justforit4102 <deeppakhare19@gmail.com>
2025-12-04 23:12:11 +0530

    learning static

---
+++ JAVASCRIPT/OOP/staticprop.js
@@ -0,0 +1,28 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Usernmae: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const deep = new User("deep")
+// console.log(deep.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+// iphone.logMe();
+console.log(iphone.createId());
+
