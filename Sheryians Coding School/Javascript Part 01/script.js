cd1db9be97a6a9cf53b355823f4b9bce06062ad9
justforit4102 <deeppakhare19@gmail.com>
2026-02-27 02:45:11 +0530

    javascript

---
+++ Sheryians Coding School/Javascript Part 01/script.js
@@ -0,0 +1,26 @@
+// let marks = 75;
+
+// let result = marks >= 90 ? "Excellent" : marks >= 70 ? "Good" : marks >= 40 ? "Average" : "Fail"
+
+// console.log("Result is : " + result);
+
+
+function calc(a, b, operator) {
+    let result =  a + operator 
+
+    console.log(result);
+    
+}
+
+console.log(calc(2, 3, +));
+
+
+
+
+
+
+
+
+
+
+
