5a4a68319443a995f2ac31d00f602b458e7a9c94
justforit4102 <deeppakhare19@gmail.com>
2026-03-08 23:22:59 +0530

    loops

--- Sheryians Coding School/Javascript Part 01/script.js
+++ Sheryians Coding School/Javascript Part 01/script.js
@@ -651,3 +651,21 @@
 // console.log(`LCM of given two numbers is ${lcm}`);
 
 
+// Remove duplicates from an array
+// let arr = [2, 3, 6, 3, 4, 1 ,5 ,1 , 3, 2];
+// let arr2 = [];
+// for(let i = 0; i <= arr.length; i++) {
+//     let found = false;
+//     for(let j = 0; j <= arr2.length; j++) {
+//         if(arr[i] === arr2[j]){
+//             found = true;
+//             break;
+//         }
+//     }
+//     if(!found) {
+//         arr2.push(arr[i]);
+//     }
+// }
+// console.log(arr2);
+
+
