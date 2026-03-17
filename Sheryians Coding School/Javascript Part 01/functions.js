764e0be91c7914d8fa26d59cee79d75e30f435b1
justforit4102 <deeppakhare19@gmail.com>
2026-03-17 16:23:53 +0530

    advanced functions

--- Sheryians Coding School/Javascript Part 01/functions.js
+++ Sheryians Coding School/Javascript Part 01/functions.js
@@ -288,12 +288,3 @@ secondLargest([244, 34, 2, 50, 125, 75, 222])
 
 
 
-// Write a function that checks if two strings are anagrams.
-function checkAnagram(str1, str2) {
-  let a = str1.split("").sort().join("");
-  let b = str2.split("").sort().join("");
-
-  return a === b;
-}
-
-console.log(checkAnagram("listen", "silent"));
